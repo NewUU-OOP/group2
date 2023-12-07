59f8e0c97ad0f262368e544aacbf72a5997a4f2a
macbookair <s.djuraev@newuzbekistanuniversity.uz>
2023-12-07 09:27:19 +0500

    V1

--- LMS/src/Main.java
+++ LMS/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome Library Management System !");
+        System.out.println("This is a second message ");
 
 
     }
